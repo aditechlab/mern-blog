00b4a4967aaf611be9d253315acbab5f1144c729
fixnetcodes <deogratiasamas@gmail.com>
2024-08-14 15:11:31 +0300

    added footer component and styles

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,8 +1,24 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer>
+      <ul className="footer__categories">
+        <li><Link to="/posts/categores/Agriculture">Agriculture</Link></li>
+        <li><Link to="/posts/categores/Business">Business</Link></li>
+        <li><Link to="/posts/categores/Education">Education</Link></li>
+        <li><Link to="/posts/categores/Entertainment">Entertainment</Link></li>
+        <li><Link to="/posts/categores/Culture">Culture</Link></li>
+        <li><Link to="/posts/categores/Politics">Politics</Link></li>
+        <li><Link to="/posts/categores/Art">Art</Link></li>
+        <li><Link to="/posts/categores/Economy">Economy</Link></li>
+        <li><Link to="/posts/categores/Wealther">Wealther</Link></li>
+      </ul>
+      <div className="footer__copyright">
+        <small>All Rights Reserved &copy; Copyright, Mern Blog</small>
+      </div>
+    </footer>
   )
 }
 
