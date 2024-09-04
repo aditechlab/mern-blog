142a00c99d30dfffb3648d5fa31e9d3a92fd2603
fixnetcodes <deogratiasamas@gmail.com>
2024-09-04 16:56:52 +0300

    Added login and register pages

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,8 +1,32 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Link } from 'react-router-dom'
 
 const Login = () => {
+  const [userData, setUserData] = useState({
+    email: '',
+    password: ''
+  })
+  
+  const changeInputHandle = (e) => {
+    setUserData(prevState => {
+      return {...prevState, [e.target.name]: e.target.value}
+    })
+  }
   return (
-    <div>Login</div>
+    <section className='login'>
+      <div className="container">
+        <h2>Sign In</h2>
+        <form action="" className="form register__form">
+          <p className="form__error-message">This is an error message</p>
+          <input type="email" name="email" id="email" placeholder='Enter email' value={userData.email}
+            onChange={changeInputHandle} />
+          <input type="password" name="password" id="password" placeholder='Enter Password' 
+            value={userData.password} onChange={changeInputHandle} />
+            <button type="submit" className='btn primary'>Login</button>
+        </form>
+        <small>Don't you have an account? <Link to='/register'>Sign up</Link></small>
+      </div>
+    </section>
   )
 }
 
--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,8 +1,38 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Link } from 'react-router-dom'
 
 const Register = () => {
+  const [userData, setUserData] = useState({
+    name: '',
+    email: '',
+    password: '',
+    password2: ''
+  })
+  
+  const changeInputHandle = (e) => {
+    setUserData(prevState => {
+      return {...prevState, [e.target.name]: e.target.value}
+    })
+  }
   return (
-    <div>Register</div>
+    <section className='register'>
+      <div className="container">
+        <h2>Sign Up</h2>
+        <form action="" className="form register__form">
+          <p className="form__error-message">This is an error message</p>
+          <input type="text" name="name" id="name" placeholder='Full Name' value={userData.name}
+            onChange={changeInputHandle} />
+          <input type="email" name="email" id="email" placeholder='Enter email' value={userData.email}
+            onChange={changeInputHandle} />
+          <input type="password" name="password" id="password" placeholder='Enter Password' 
+            value={userData.password} onChange={changeInputHandle} />
+          <input type="password" name="password2" id="password2" placeholder='Enter Password confirmation' 
+            value={userData.password} onChange={changeInputHandle} />
+            <button type="submit" className='btn primary'>Register</button>
+        </form>
+        <small>Already have an account? <Link to='/login'>Sign in</Link></small>
+      </div>
+    </section>
   )
 }
 
