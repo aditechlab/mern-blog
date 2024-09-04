a2fc717213d794b11957cec140629be36afbc2ca
fixnetcodes <deogratiasamas@gmail.com>
2024-09-04 12:40:32 +0300

    Update CategoryPosts.jsx

--- client/src/pages/CategoryPosts.jsx
+++ client/src/pages/CategoryPosts.jsx
@@ -1,8 +1,19 @@
-import React from 'react'
+import React, { useState } from 'react'
+
+import PostItem from '../components/PostItem'
+import { DUMMY_POSTS } from '../data'
 
 const CategoryPosts = () => {
+  const [posts, setPosts] = useState(DUMMY_POSTS)
   return (
-    <div>CategoryPosts</div>
+    <section className='posts'>
+        {posts.length > 0 ? <div className="container posts__container">
+            {
+                posts.map(({id, thumbnail, category,title,desc, authorID}) => 
+                <PostItem key={id} postID={id} thumbnail={thumbnail} title={title} category={category} description={desc} authorID={authorID} />)
+            }
+        </div> : <h2 className='center'>No posts found</h2>}
+    </section>
   )
 }
 
