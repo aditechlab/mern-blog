56b2872ad93e4a7ea318904c89a0ad8742e8f000
amasdeogratias <deogratiasprog@gmail.com>
2024-10-02 12:26:10 +0300

    added autopopulate categories in footer

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -2,18 +2,15 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 
 const Footer = () => {
+
+  const POST_CATEGORIES = ["Agriculture","Business","Education","Entertainment","Art","Investment","Weather"];
+
   return (
     <footer>
       <ul className="footer__categories">
-        <li><Link to="/posts/categores/Agriculture">Agriculture</Link></li>
-        <li><Link to="/posts/categores/Business">Business</Link></li>
-        <li><Link to="/posts/categores/Education">Education</Link></li>
-        <li><Link to="/posts/categores/Entertainment">Entertainment</Link></li>
-        <li><Link to="/posts/categores/Culture">Culture</Link></li>
-        <li><Link to="/posts/categores/Politics">Politics</Link></li>
-        <li><Link to="/posts/categores/Art">Art</Link></li>
-        <li><Link to="/posts/categores/Economy">Economy</Link></li>
-        <li><Link to="/posts/categores/Wealther">Wealther</Link></li>
+        {POST_CATEGORIES.map(cat => 
+          <li><Link to={`/posts/categories/${cat}`}>{cat}</Link></li>
+        )}
       </ul>
       <div className="footer__copyright">
         <small>All Rights Reserved &copy; Copyright, Mern Blog</small>
