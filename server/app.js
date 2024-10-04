46941ec80d9bd9d5eec481653233bd41cb47b978
amasdeogratias <deogratiasprog@gmail.com>
2024-10-04 17:24:28 +0300

    added database connection

--- server/app.js
+++ server/app.js
@@ -4,6 +4,9 @@ const { connect } = require('mongoose');
 require('dotenv').config();
 
 const app = express();
+
+//connect to db
+connect(process.env.MONGO_URI).then(
 app.listen(5000, () => {
-    console.log('Server started at port 5000')
-});
+    console.log(`Server started at port ${process.env.PORT}`)
+})).catch(error => {console.log(error)});
