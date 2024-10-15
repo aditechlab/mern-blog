57d1fafb99855192e165d51369c0b7b57bba2e79
amasdeogratias <deogratiasprog@gmail.com>
2024-10-15 12:51:44 +0300

    Added user register implementation

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,8 +1,41 @@
 //===============Register user
 // POST: api/users/register
+
+const HttpError = require("../models/errorModel");
+const User = require("../models/userModel");
+
 //unprotected
-const registerUser = (req, res, next) => {
-    res.json('Register user');
+const registerUser = async (req, res, next) => {
+    try{
+        //requests from user
+        const {name, email, password, password2, avatar} = req.body;
+
+        if(!name || !email || !password){
+            return next(new HttpError("Fill all fields. ", 422));
+        }
+
+        //change email to lower case
+        const newEmail = email.toLowerCase();
+
+        const emailExists = await User.findOne({email:newEmail});
+        if(emailExists){
+            return next(new HttpError('User with this email exists, use different email', 422));
+        }
+
+        if((password.trim()).length < 6){
+            return next(new HttpError('Password must be atleast 6 characters and more. ', 422));
+        }
+
+        if(password != password2){
+            return next(new HttpError("Passwords do not match!. ", 422));
+        }
+        
+        const newUser = await User.create({name, email: newEmail, password});
+        res.status(201).json(newUser);
+
+    }catch(error){
+        return next(new HttpError("User registration failed.", 422));
+    }
 }
 
 //===============Login a Registered user
