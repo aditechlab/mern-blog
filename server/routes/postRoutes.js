b64ae57ea7ef2a872286aaa2d29f94f3a81dcf3a
DeogratiasAmas <deogratiasprog@gmail.com>
2024-11-29 14:24:51 +0300

    added routes for posts endpoints

--- server/routes/postRoutes.js
+++ server/routes/postRoutes.js
@@ -1,10 +1,24 @@
 const express = require('express');
 
+const { 
+    getPosts, 
+    getSinglePosts, 
+    getCategoryPosts, 
+    getAuthorPosts, 
+    createPost,
+    editPost,
+    deletePost } = require('../controllers/postController');
+const authMiddleware = require('../middleware/authMiddleware')
+
 const router = express.Router();
 
 //define routes methods
-router.get('/', (req, res, next) => {
-    res.json('This is the post routes');
-})
+router.get('/', getPosts);
+router.get('/:id', getSinglePosts);
+router.get('/categories/:category', getCategoryPosts);
+router.get('/users/:id', getAuthorPosts);
+router.post('/create', authMiddleware, createPost);
+router.patch('/edit-post', authMiddleware, editPost);
+router.delete('/:id', authMiddleware, deletePost)
 
 module.exports = router;
