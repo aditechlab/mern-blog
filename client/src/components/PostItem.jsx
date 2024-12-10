b753f3f8e23053cf49a15f367dcdb5af005c101a
DeogratiasAmas <deogratiasprog@gmail.com>
2024-12-10 14:36:07 +0300

    Fetched posts from db and displayed them

--- client/src/components/PostItem.jsx
+++ client/src/components/PostItem.jsx
@@ -2,13 +2,14 @@ import React from 'react'
 import {Link} from 'react-router-dom'
 import PostAuthor from './PostAuthor'
 
-const PostItem = ({postID, category, title, description, authorID, thumbnail}) => {
+const PostItem = ({postID, thumbnail, title, category,  description, authorID, createdAt}) => {
   const shortDescription = description.length > 145 ? description.substring(0, 145) + '...' : description
   const postTitle = title.length > 30 ? title.substring(0, 30) + '...' : title
+
   return (
     <article className='post'>
         <div className="post__thumbnail">
-            <img src={thumbnail} alt={title} />
+            <img src={`${process.env.REACT_APP_ASSETS_BASE_URL}/uploads/${thumbnail}`} alt={title} />
         </div>
         <div className="post__content">
             <Link to={`/posts/${postID}`}>
@@ -17,7 +18,7 @@ const PostItem = ({postID, category, title, description, authorID, thumbnail}) =
         </div>
         <p>{shortDescription}</p>
         <div className="post__footer">
-            <PostAuthor />
+            <PostAuthor authorID={authorID} createdAt={createdAt}/>
             <Link to={`/posts/categories/${category}`} className='btn category'>{category}</Link>
         </div>
       
