b6e3ff7f6dc8f9c825b9324505ad04f1d26e37b0
DeogratiasAmas <deogratiasprog@gmail.com>
2024-12-31 15:38:24 +0300

    Get post details from the db by id in edit post form

--- client/src/pages/EditPost.jsx
+++ client/src/pages/EditPost.jsx
@@ -1,13 +1,24 @@
-import React, { useState } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import ReactQuill from 'react-quill';
 import 'react-quill/dist/quill.snow.css';
 import AccessControl from '../components/AccessControl';
+import { useNavigate, useParams } from 'react-router-dom';
+import { UserContext } from '../context/userContext';
+import axios from 'axios';
 
 const EditPost = () => {
   const [title, setTitle] = useState('');
   const [category, setCategory] = useState('Uncategorized');
   const [description, setDescription] = useState('');
   const [thumbnail, setThumbnail] = useState('');
+  const [error, setError] = useState('')
+
+  const navigate = useNavigate()
+  const {id} = useParams()
+
+
+  const {currentUser} = useContext(UserContext);
+  const token = currentUser?.token
 
   const modules = {
     toolbar: [
@@ -27,14 +38,28 @@ const EditPost = () => {
 
   const POST_CATEGORIES = ["Agriculture","Business","Education","Entertainment","Art","Investment","Uncategorized","Weather"];
 
+
+  useEffect(()=> {
+    const getPost = async () => {
+      try {
+        const response = await axios.get(`${process.env.REACT_APP_BASE_URL}/posts/${id}`);
+        setTitle(response.data.title)
+        setCategory(response.data.category)
+        setDescription(response.data.description)
+        
+      } catch (err) {
+        setError(err.response.data.message);
+      }
+    }
+    getPost();
+  }, [])
+
   return (
     <section className="create-post">
       <AccessControl />
       <div className="container">
         <h2>Edit Post</h2>
-        <p className="form__error-message">
-          This is an error message
-        </p>
+        {error && <p className="form__error-message">{error}</p>}
         <form action="" className="form create-post__form">
           <input type="text" name="title" placeholder='Title' id="title" value={title} onChange={e => setTitle(e.target.value)} autoFocus />
           <select name='category' value={category} onChange={e=>setCategory(e.target.value)}>
