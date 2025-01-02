64d05d91993b8d189dd8bd022a75943d3d037908
DeogratiasAmas <deogratiasprog@gmail.com>
2025-01-02 12:29:03 +0300

    Added a loading state in delete post

--- client/src/pages/DeletePost.jsx
+++ client/src/pages/DeletePost.jsx
@@ -1,8 +1,9 @@
-import React, { useContext } from 'react'
+import React, { useContext, useState } from 'react'
 import AccessControl from '../components/AccessControl'
 import { Link, useLocation, useNavigate } from 'react-router-dom'
 import axios from 'axios';
 import { UserContext } from '../context/userContext';
+import Loader from '../components/Loader';
 
 const DeletePost = ({postID: id}) => {
 
@@ -10,10 +11,12 @@ const DeletePost = ({postID: id}) => {
   const {currentUser} = useContext(UserContext);
   const token = currentUser?.token
   const location = useLocation();
+   const [isLoading, setIsLoading] = useState(false);
 
   console.log(id);
   
   const removePost = async () => {
+    setIsLoading(true)
     try {
       const response = await axios.delete(`${process.env.REACT_APP_BASE_URL}/posts/${id}`, {
           withCredentials:true, headers: {Authorization: `Bearer ${token}`} 
@@ -22,16 +25,21 @@ const DeletePost = ({postID: id}) => {
         
         if(response.status === 200){
           //check location where the post is deleted
-          if(location.pathname === `myposts/${currentUser.id}`){
+          if(location.pathname === `/myposts/${currentUser.id}`){
             navigate(0);
           }else{
             navigate('/');
           }
         }
+        setIsLoading(false)
     } catch (err) {
       console.log(err);
       
     }
+  }
+
+  if(isLoading){
+    return <Loader />
   }
 
   return (
